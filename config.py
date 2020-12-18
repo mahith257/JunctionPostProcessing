129531f5f6106ef79350e7e3b7034e1727068edb
BEKKEM <f20170028@hyderabad.bits-pilani.ac.in>
2020-12-18 17:05:02 +0530

    'config'

---
+++ config.py
@@ -0,0 +1,9 @@
+INSTANCE_MASK_PATH = 'erode_instance_images_new/*.jpg'
+SLICING_IMAGES_PATH = 'erode_instance_images_new/'
+SLICED_TILES_PATH = 'erode_instance_sliced_tiles_new/'
+CONCATENATED_IMAGES_PATH = 'concatenated_erode_instance_images_new/'
+SOURCE_IMAGE_PATH = 'source_image/'
+VISUALIZED_IMAGES_PATH = 'visualized_erode_instance_images_new/'
+
+
+
